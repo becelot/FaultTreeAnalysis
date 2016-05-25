b54bce766d0dcb03a4518f6d63113bde7f15c1b8
BBecker <benedict.becker93@rwth-aachen.de>
2016-05-25 16:00:42 +0200

    Initial programming project

---
+++ FaultTreeAnalysis/MainProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaultTreeAnalysis
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
