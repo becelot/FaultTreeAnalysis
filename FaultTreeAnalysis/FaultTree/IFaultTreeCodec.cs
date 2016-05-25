6d57c7165b42ea3f0e5e763a96f64058f2ec4b26
BBecker <benedict.becker93@rwth-aachen.de>
2016-05-25 16:38:38 +0200

    Created main FaultTree reader components

---
+++ FaultTreeAnalysis/FaultTree/IFaultTreeCodec.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaultTreeAnalysis.FaultTree
+{
+    public enum FaultTreeFormat
+    {
+        FAULT_TREE_UNKNOWN = 0,
+        FAULT_TREE_DOT,
+        FAULT_TREE_XML
+    };
+
+    public abstract class IFaultTreeCodec
+    {
+
+        public abstract void write(FaultTree ft);
+
+        public abstract FaultTree read(String fileName);
+
+        public virtual FaultTreeFormat getFormatToken() { return FaultTreeFormat.FAULT_TREE_UNKNOWN; }
+    }
+}
---
+++ FaultTreeAnalysis/FaultTree/XmlFaultTreeCodec.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaultTreeAnalysis.FaultTree
+{
+    class XmlFaultTreeCodec : IFaultTreeCodec
+    {
+        public override FaultTree read(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void write(FaultTree ft)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override FaultTreeFormat getFormatToken()
+        {
+            return FaultTreeFormat.FAULT_TREE_XML;
+        }
+    }
+}
