7c74a764738927869bb1dc6f852888a21ed3cfd8
BBecker <benedict.becker93@rwth-aachen.de>
2016-05-25 17:30:47 +0200

    Defined Regex pattern for Dot parsing

--- FaultTreeAnalysis/FaultTree/DotFaultTreeEncoder.cs
+++ FaultTreeAnalysis/FaultTree/DotFaultTreeEncoder.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FaultTreeAnalysis.FaultTree
 {
+    enum DotParseToken
+    {
+        DOT_TRANSITION,
+        DOT_ROOT,
+        DOT_GATE,
+        DOT_IDENTIFIER
+    }
+
     class DotFaultTreeEncoder : IFaultTreeCodec
     {
+        private static Regex transition = new Regex(@"(?<from>\d*)[^-]*-\>[^\d]*(?<to>\d*).*");
+        private static Regex rootPattern = new Regex(@"(?<id>\d*)[^\[]*\[shape=point.*");
+        private static Regex gatePattern = new Regex("(?<id>\\d*)[^\\[]*\\[shape=box.*label=\"(?<typ>.*)\".*");
+        private static Regex identifierPattern = new Regex("(?<id>\\d*)[^\\[]*\\[shape=circle.*label=\"(?<identifier>.*)\".*");
+
         public override FaultTree read(StreamReader stream)
         {
-            throw new NotImplementedException();
+            FaultTree ft = null;
+
+            List<String> lines = stream.ReadToEnd().Split('\n').ToList();
+            lines = lines.GetRange(1, lines.Count() - 3).SelectMany(l => l.Split(';')).Where(l => !l.Trim().Equals("")).ToList();
+            
+
+
+            return ft;
         }
 
         public override void write(FaultTree ft, StreamWriter stream)
