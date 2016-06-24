3e4500406cb0d0c7b8546bd8e78e3dd2d1201e1c
BBecker <benedict.becker93@rwth-aachen.de>
2016-06-24 11:11:49 +0200

    Added basic gate connection tool

--- FaultTreeAnalysis.GUI/MainWindow.xaml.cs
+++ FaultTreeAnalysis.GUI/MainWindow.xaml.cs
@@ -78,50 +78,53 @@ namespace FaultTreeAnalysis.GUI
 
 		private void FaultTreeZoomControl_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
 		{
-			var canvas = (Grid)sender;
+            Console.WriteLine("Down");
+			var canvas = FaultTreeGridView;
 
-			if (canvas.CaptureMouse())
-			{
-				var startPoint = e.GetPosition(canvas);
-				var line = new Line
-				{
-					Stroke = Brushes.Blue,
-					StrokeThickness = 3,
-					X1 = startPoint.X,
-					Y1 = startPoint.Y,
-					X2 = startPoint.X,
-					Y2 = startPoint.Y,
-				};
+            var canvasLine = canvas.Children.OfType<Line>().LastOrDefault();
+            if (canvasLine == null)
+            {
+                var startPoint = e.GetPosition(canvas);
+                var line = new Line
+                {
+                    Stroke = Brushes.Blue,
+                    StrokeThickness = 3,
+                    X1 = startPoint.X,
+                    Y1 = startPoint.Y,
+                    X2 = startPoint.X,
+                    Y2 = startPoint.Y,
+                };
 
-				canvas.Children.Add(line);
-			}
-		}
+                canvas.Children.Add(line);
+            } else
+            {
+                canvas.Children.Remove(canvasLine);
+            }
+        }
 
 		private void FaultTreeZoomControl_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
 		{
-			((Grid)sender).ReleaseMouseCapture();
-
-			var canvas = (Grid)sender;
-
-			var line = canvas.Children.OfType<Line>().LastOrDefault();
-			canvas.Children.Remove(line);
 		}
 
 		private void FaultTreeZoomControl_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
 		{
-			var canvas = (Grid)sender;
+            
+			var canvas = FaultTreeGridView;
 
-			if (canvas.IsMouseCaptured && e.LeftButton == MouseButtonState.Pressed)
-			{
-				var line = canvas.Children.OfType<Line>().LastOrDefault();
-
-				if (line != null)
-				{
-					var endPoint = e.GetPosition(canvas);
-					line.X2 = endPoint.X;
-					line.Y2 = endPoint.Y;
-				}
-			}
-		}
+            var line = canvas.Children.OfType<Line>().LastOrDefault();
+            if (line == null)
+            {
+                return;
+            }
+            if (line != null)
+            {
+                
+                var endPoint = e.GetPosition(canvas);
+                var directed = endPoint - new Point(line.X1, line.Y1);
+                directed.Normalize();
+                line.X2 = endPoint.X - directed.X * 2;
+                line.Y2 = endPoint.Y - directed.Y * 2;
+            }
+        }
 	}
 }
