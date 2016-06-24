f438b02d8417367ab6072f4a35b76b13ed6709e3
BBecker <benedict.becker93@rwth-aachen.de>
2016-06-24 11:16:17 +0200

    Refactored VisualEditor

--- FaultTreeAnalysis.GUI/MainWindow.xaml.cs
+++ FaultTreeAnalysis.GUI/MainWindow.xaml.cs
@@ -72,56 +72,5 @@ namespace FaultTreeAnalysis.GUI
 				this.LoadFromFile(filename);
 			}
 		}
-
-		private void FaultTreeZoomControl_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-		{
-            Console.WriteLine("Down");
-			var canvas = FaultTreeGridView;
-
-            var canvasLine = canvas.Children.OfType<Line>().LastOrDefault();
-            if (canvasLine == null)
-            {
-                var startPoint = e.GetPosition(canvas);
-                var line = new Line
-                {
-                    Stroke = Brushes.Blue,
-                    StrokeThickness = 3,
-                    X1 = startPoint.X,
-                    Y1 = startPoint.Y,
-                    X2 = startPoint.X,
-                    Y2 = startPoint.Y,
-                };
-
-                canvas.Children.Add(line);
-            } else
-            {
-                canvas.Children.Remove(canvasLine);
-            }
-        }
-
-		private void FaultTreeZoomControl_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
-		{
-		}
-
-		private void FaultTreeZoomControl_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
-		{
-            
-			var canvas = FaultTreeGridView;
-
-            var line = canvas.Children.OfType<Line>().LastOrDefault();
-            if (line == null)
-            {
-                return;
-            }
-            if (line != null)
-            {
-                
-                var endPoint = e.GetPosition(canvas);
-                var directed = endPoint - new Point(line.X1, line.Y1);
-                directed.Normalize();
-                line.X2 = endPoint.X - directed.X * 2;
-                line.Y2 = endPoint.Y - directed.Y * 2;
-            }
-        }
 	}
 }
---
+++ FaultTreeAnalysis.GUI/VisualEditor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace FaultTreeAnalysis.GUI
+{
+    public partial class MainWindow
+    {
+        private void FaultTreeZoomControl_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var canvas = FaultTreeGridView;
+
+            var canvasLine = canvas.Children.OfType<Line>().LastOrDefault();
+            if (canvasLine == null)
+            {
+                var startPoint = e.GetPosition(canvas);
+                var line = new Line
+                {
+                    Stroke = Brushes.Blue,
+                    StrokeThickness = 3,
+                    X1 = startPoint.X,
+                    Y1 = startPoint.Y,
+                    X2 = startPoint.X,
+                    Y2 = startPoint.Y,
+                };
+
+                canvas.Children.Add(line);
+            }
+            else
+            {
+                canvas.Children.Remove(canvasLine);
+            }
+        }
+
+        private void FaultTreeZoomControl_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+        }
+
+        private void FaultTreeZoomControl_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
+            var canvas = FaultTreeGridView;
+
+            var line = canvas.Children.OfType<Line>().LastOrDefault();
+            if (line == null)
+            {
+                return;
+            }
+            if (line != null)
+            {
+
+                var endPoint = e.GetPosition(canvas);
+                var directed = endPoint - new Point(line.X1, line.Y1);
+                directed.Normalize();
+                line.X2 = endPoint.X - directed.X * 2;
+                line.Y2 = endPoint.Y - directed.Y * 2;
+            }
+        }
+    }
+}
