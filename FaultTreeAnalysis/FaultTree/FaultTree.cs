9d11062dbd7f66fec6bbeece306031ab065a5d57
BBecker <benedict.becker@rwth-aachen.de>
2016-06-26 22:35:44 +0200

    Added MarkovChain serialization

--- FaultTreeAnalysis/FaultTree/FaultTree.cs
+++ FaultTreeAnalysis/FaultTree/FaultTree.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.Serialization;
 using FaultTreeAnalysis.FaultTree.MarkovChain;
 using FaultTreeAnalysis.FaultTree.Transformer;
 using FaultTreeAnalysis.FaultTree.Tree;
+using MathNet.Numerics.LinearAlgebra;
 
 namespace FaultTreeAnalysis.FaultTree
 {
@@ -12,10 +15,25 @@ namespace FaultTreeAnalysis.FaultTree
         [DataMember]
         public FaultTreeNode Root { get; set; }
 
-		[DataMember]
 		public MarkovChain<FaultTreeTerminalNode> MarkovChain { get; set; }
 
-        public FaultTree()
+	    [DataMember]
+	    private IEnumerable<Tuple<FaultTreeTerminalNode, double, FaultTreeTerminalNode>> MarkovArray
+	    {
+		    get { return MarkovChain.GetAllEdges();}
+		    set
+		    {
+			    int row = value.Select(v => v.Item1).Union(value.Select(v => v.Item3)).GroupBy(v => v).Count();
+			    //MarkovChain = new MarkovChain<FaultTreeTerminalNode>(Matrix<double>.Build.DenseOfIndexed(row, row, value));
+				MarkovChain = new MarkovChain<FaultTreeTerminalNode>(row);
+			    foreach (var trans in value)
+			    {
+				    MarkovChain[trans.Item1, trans.Item3] = trans.Item2;
+			    }
+		    }
+	    }
+
+	    public FaultTree()
         {
 
         }
