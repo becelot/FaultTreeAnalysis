468b0757b976177d2fef68920823aabd91ac7f51
BBecker <benedict.becker93@rwth-aachen.de>
2016-06-26 12:11:52 +0200

    Added Testing framework based on NUnit

---
+++ FaultTreeAnalysis.Tests/FaultTree/MarkovChain/MatrixUniformizationTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using FaultTreeAnalysis.FaultTree.MarkovChain;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Single;
+
+namespace FaultTreeAnalysis.Tests.FaultTree.MarkovChain
+{
+    [TestFixture()]
+    public class MatrixUniformizationTests
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(Matrix<double>.Build.DenseOfArray(new double[,] { {-0.5, 0.6}, {0.5, 0.6} })).Returns(0);
+            }
+        }
+
+        [Test, TestCaseSource(typeof(MatrixUniformizationTests), "TestCases")]
+        public double UniformizationTest(Matrix<double> matrix)
+        {
+            return 0;
+        }
+    }
+
+    public class UniformizationTestCases
+    {
+        
+    }
+}
