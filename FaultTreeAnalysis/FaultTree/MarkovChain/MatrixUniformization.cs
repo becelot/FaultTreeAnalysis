e414be375061c0ecf32c11359787279f0cbd5422
BBecker <benedict.becker@rwth-aachen.de>
2016-06-26 15:17:44 +0200

    Removed unused directives

--- FaultTreeAnalysis/FaultTree/MarkovChain/MatrixUniformization.cs
+++ FaultTreeAnalysis/FaultTree/MarkovChain/MatrixUniformization.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MathNet.Numerics.LinearAlgebra;
 
 namespace FaultTreeAnalysis.FaultTree.MarkovChain
@@ -24,11 +20,11 @@ namespace FaultTreeAnalysis.FaultTree.MarkovChain
 		        }
 	        }
 
-	        Matrix<double> Qprime = matrix/q + Matrix<double>.Build.DenseIdentity(2,2);
+	        Matrix<double> qprime = matrix/q + Matrix<double>.Build.DenseIdentity(2,2);
 
 			var sum = 0.0d;
-	        int l = 1;
-			for (l = 1; true; l++)
+	        int l;
+			for (l = 1;; l++)
 	        {
 				sum += Math.Pow(q * time, l-1) / Factorial(l-1);
 				if (Math.Exp(-q*time)*sum <= errorTolerance/2)
@@ -39,11 +35,11 @@ namespace FaultTreeAnalysis.FaultTree.MarkovChain
 	        }
 	        l--;
 
-	        int k = 1;
+	        int k;
 	        if (l > 0)
 	        {
 		        sum = 1;
-		        for (k = 1; true; k++)
+		        for (k = 1;; k++)
 		        {
 			        sum += Math.Pow(q*time, k)/Factorial(k);
 			        if ((1 - Math.Exp(-q*time)*sum) > errorTolerance/2)
@@ -56,7 +52,7 @@ namespace FaultTreeAnalysis.FaultTree.MarkovChain
 	        else
 	        {
 				sum = 1;
-				for (k = 1; true; k++)
+				for (k = 1;; k++)
 				{
 					sum += Math.Pow(q * time, k) / Factorial(k);
 					if ((1 - Math.Exp(-q * time) * sum) > errorTolerance)
@@ -67,17 +63,17 @@ namespace FaultTreeAnalysis.FaultTree.MarkovChain
 				}
 			}
 
-	        Vector<double> S = Vector<double>.Build.Dense(matrix.RowCount);
-	        Vector<double> Pl = initialDistributon*Qprime.Power(l);
-	        Vector<double> R = Pl*Math.Pow(q*time, l)/Factorial(l);
+	        Vector<double> s = Vector<double>.Build.Dense(matrix.RowCount);
+	        Vector<double> pl = initialDistributon*qprime.Power(l);
+	        Vector<double> r = pl*Math.Pow(q*time, l)/Factorial(l);
 
 	        for (int i = l+1; i <= k; i++)
 	        {
-		        S = S + R;
-		        R = R*Qprime*q*time/ Math.Max(i, 1);
+		        s = s + r;
+		        r = r*qprime*q*time/ Math.Max(i, 1);
 	        }
 
-            return (S * Math.Exp(-q * time));
+            return (s * Math.Exp(-q * time));
         }
 
 	    public static long Factorial(int l)
