e15c2a35ceaae46b827a7f5071193a72e69e4805
BBecker <benedict.becker93@rwth-aachen.de>
2016-06-26 12:05:13 +0200

    Added Matrix Uniformization extension method [no implementation]

---
+++ FaultTreeAnalysis/FaultTree/MarkovChain/MatrixUniformization.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace FaultTreeAnalysis.FaultTree.MarkovChain
+{
+    public static class MatrixUniformization
+    {
+        public static double Uniformization(this Matrix<double> matrix, double time, double errorTolerance)
+        {
+            return 0;
+        }
+    }
+}
