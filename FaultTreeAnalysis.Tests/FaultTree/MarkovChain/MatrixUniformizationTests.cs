0be39c2725cd798fa96d81bee31ed0b914056ee7
BBecker <benedict.becker@rwth-aachen.de>
2016-06-26 21:10:00 +0200

    Fixed testcase not resulting in accurate comparisons

--- FaultTreeAnalysis.Tests/FaultTree/MarkovChain/MatrixUniformizationTests.cs
+++ FaultTreeAnalysis.Tests/FaultTree/MarkovChain/MatrixUniformizationTests.cs
@@ -13,30 +13,40 @@ namespace FaultTreeAnalysis.Tests.FaultTree.MarkovChain
         {
 	        get
 	        {
-		        yield return TestCaseBuilder(0.5, 0.6, 0.2, 1e-16);
-				yield return TestCaseBuilder(0.5, 0.6, 0.1, 1e-16);
-				yield return TestCaseBuilder(0.5, 0.6, 0.3, 1e-16);
-				yield return TestCaseBuilder(0.5, 0.6, 2.0, 1e-16);
-				yield return TestCaseBuilder(0.5, 0.6, 20.0, 1e-16);
+		        yield return TestCaseBuilder(0.5, 0.6, 0.2, 1e-10);
+				yield return TestCaseBuilder(0.5, 0.6, 35.0d, 1e-6);
+				yield return TestCaseBuilder(0.5, 0.6, 0.1, 1e-10);
+				yield return TestCaseBuilder(0.5, 0.6, 0.3, 1e-10);
+				yield return TestCaseBuilder(0.5, 0.6, 2.0, 1e-10);
+				yield return TestCaseBuilder(0.5, 0.6, 20.0, 1e-10);
+				yield return TestCaseBuilder(0.4, 0.6, 0.1, 1e-10);
+				yield return TestCaseBuilder(0.4, 0.2, 0.1, 1e-10);
+				yield return TestCaseBuilder(12.0, 0.6, 0.1, 1e-10);
+				yield return TestCaseBuilder(3.4, 0.1, 0.1, 1e-10);
+				yield return TestCaseBuilder(3.4, 12.1, 0.7, 1e-10);
 			}
         }
 
 	    private static TestCaseData TestCaseBuilder(double lambda, double mu, double time, double tolerance)
 	    {
-			return new TestCaseData(Matrix<double>.Build.DenseOfArray(new[,] { { -lambda, lambda }, { mu, -mu } }), time, tolerance).Returns(true);
+			return new TestCaseData(Matrix<double>.Build.DenseOfArray(new[,] { { -lambda, lambda }, { mu, -mu } }), time, tolerance);
 		}
 
 		[Test, TestCaseSource(typeof(MatrixUniformizationTests), nameof(TestCases))]
-        public bool UniformizationTest(Matrix<double> matrix, double time, double tolerance)
+        public void UniformizationTest(Matrix<double> matrix, double time, double tolerance)
         {
 	        var goal = matrix.Uniformization(Vector<double>.Build.DenseOfArray(new[] { 1.0d, 0.0d}), time, tolerance);
 
-	        var lambda = matrix[1,0];
+	        var lambda = matrix[0,1];
 	        var mu = matrix[1,0];
 
 	        var test = mu/(lambda + mu) + (lambda/(lambda + mu)*Math.Exp(-(lambda + mu)*time));
 
-			return test - goal[0] < tolerance*10;
+			if (!double.IsPositiveInfinity(matrix.ConditionNumber()))
+			{
+				Assert.That(goal[0], Is.EqualTo(test).Within(tolerance * 100));
+			}
+			
         }
     }
 }
