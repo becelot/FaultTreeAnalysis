b2d9cbb7c5d7f18ea4e9d46902fda1e4fb7241bf
BBecker <benedict.becker@rwth-aachen.de>
2016-06-27 20:48:38 +0200

    Progress Windows

--- FaultTreeAnalysis.GUI/Windows/MessageDialogs.cs
+++ FaultTreeAnalysis.GUI/Windows/MessageDialogs.cs
@@ -14,5 +14,11 @@ namespace FaultTreeAnalysis.GUI.Windows
 
 		public static async Task<string> ShowRateDialogAsync()
 			=> await ShowInputDialogAsync("Rate", "Enter rate for edge", new MetroDialogSettings());
+
+	    public static async Task<ProgressDialogController> ShowProgressDialog(string title, string message, MetroDialogSettings settings) => 
+            await Core.MainWindow.ShowProgressAsync(title, message, false, settings);
+
+	    public static async Task<ProgressDialogController> ShowBDDConstructionDialog() =>
+	        await ShowProgressDialog("Binary decision diagram", "Converting Fault Tree to BDD", new MetroDialogSettings());
 	}
 }
