22bfe82eb995ad8c2ff5ffdc713730acc25674ca
BBecker <benedict.becker@rwth-aachen.de>
2016-06-27 12:58:19 +0200

    Used core class initializer

--- FaultTreeAnalysis.GUI/App.xaml.cs
+++ FaultTreeAnalysis.GUI/App.xaml.cs
@@ -1,7 +1,13 @@
 ﻿
+using System.Windows;
+
 namespace FaultTreeAnalysis.GUI
 {
     public partial class App
     {
+	    private void App_OnStartup(object sender, StartupEventArgs e)
+	    {
+		    Core.Initialize();
+	    }
     }
 }
