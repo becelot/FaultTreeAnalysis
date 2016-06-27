5ecf7d2af8cc259dbe11df3f99e9ae576d93f27a
BBecker <benedict.becker@rwth-aachen.de>
2016-06-27 12:57:35 +0200

    Added Core wrapper that will initialize all components

---
+++ FaultTreeAnalysis.GUI/Core.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FaultTreeAnalysis.GUI.Windows;
+
+namespace FaultTreeAnalysis.GUI
+{
+	public static class Core
+	{
+		public static MainWindow MainWindow { get; set; }
+
+		public static void Initialize()
+		{
+			MainWindow = new MainWindow();
+			MainWindow.Show();
+		}
+	}
+}
