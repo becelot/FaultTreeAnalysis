f381078a41b31e6f712409815674a152d5192d1e
BBecker <benedict.becker@rwth-aachen.de>
2016-06-27 13:11:16 +0200

    Added MessageDialog wrapper

---
+++ FaultTreeAnalysis.GUI/Windows/MessageDialogs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace FaultTreeAnalysis.GUI.Windows
+{
+	public static class MessageDialogs
+	{
+		public static async Task<string> ShowInputDialogAsync(string title, string body, MetroDialogSettings settings)
+			=> await Core.MainWindow.ShowInputAsync(title, body, settings);
+
+		public static async Task<string> ShowRateDialogAsync()
+			=> await ShowInputDialogAsync("Rate", "Enter rate for edge", new MetroDialogSettings());
+	}
+}
