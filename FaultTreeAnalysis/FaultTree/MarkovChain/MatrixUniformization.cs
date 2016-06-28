2cc18fb52ef05a6b133c6829c7af7394b5b6fd1e
BBecker <benedict.becker@rwth-aachen.de>
2016-06-28 17:28:52 +0200

    Added ability to produce matrix uniformization matrix

--- FaultTreeAnalysis/FaultTree/MarkovChain/MatrixUniformization.cs
+++ FaultTreeAnalysis/FaultTree/MarkovChain/MatrixUniformization.cs
@@ -20,7 +20,7 @@ namespace FaultTreeAnalysis.FaultTree.MarkovChain
 		        }
 	        }
 
-	        Matrix<double> qprime = matrix/q + Matrix<double>.Build.DenseIdentity(2,2);
+	        Matrix<double> qprime = matrix/q + Matrix<double>.Build.DenseIdentity(matrix.RowCount, matrix.ColumnCount);
 
 			var sum = 0.0d;
 	        int l;
@@ -64,13 +64,78 @@ namespace FaultTreeAnalysis.FaultTree.MarkovChain
 	        for (int i = l+1; i <= k; i++)
 	        {
 		        s = s + r;
-		        r = r*qprime*q*time/ Math.Max(i, 1);
+		        r = r*qprime*q*time/ i;
 	        }
 
             return (s * Math.Exp(-q * time));
         }
 
-	    public static long Factorial(int l)
+		public static Matrix<double> Uniformization(this Matrix<double> matrix, double time, double errorTolerance)
+		{
+
+			double q = double.MinValue;
+			for (int i = 0; i < matrix.RowCount; i++)
+			{
+				for (int j = 0; j < matrix.ColumnCount; j++)
+				{
+					if (Math.Abs(matrix[i, j]) > q)
+					{
+						q = Math.Abs(matrix[i, j]);
+					}
+				}
+			}
+
+			Matrix<double> qprime = matrix / q + Matrix<double>.Build.DenseIdentity(matrix.RowCount, matrix.ColumnCount);
+
+			var sum = 0.0d;
+			int l;
+			for (l = 1; ; l++)
+			{
+				sum += Math.Pow(q * time, l - 1) / Factorial(l - 1);
+				if (Math.Exp(-q * time) * sum <= errorTolerance / 2)
+				{
+					continue;
+				}
+				break;
+			}
+			l--;
+
+			int k;
+			if (l > 0)
+			{
+				sum = 1;
+				k = 0;
+				while ((1 - Math.Exp(-q * time) * sum) > errorTolerance / 2)
+				{
+					k++;
+					sum += Math.Pow(q * time, k) / Factorial(k);
+				}
+			}
+			else
+			{
+				sum = 1;
+				k = 0;
+				while ((1 - Math.Exp(-q * time) * sum) > errorTolerance)
+				{
+					k++;
+					sum += Math.Pow(q * time, k) / Factorial(k);
+				}
+			}
+
+			Matrix<double> s = Matrix<double>.Build.Dense(matrix.RowCount, matrix.ColumnCount);
+			Matrix<double> pl = qprime.Power(l);
+			Matrix<double> r = pl * Math.Pow(q * time, l) / Factorial(l);
+
+			for (int i = l + 1; i <= k; i++)
+			{
+				s = s + r;
+				r = r * qprime * q * time / i;
+			}
+
+			return (s * Math.Exp(-q * time));
+		}
+
+		public static long Factorial(int l)
 	    {
 		    long res = 1L;
 
