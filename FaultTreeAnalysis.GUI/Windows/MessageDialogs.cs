691aa128fb0d1c4ecdbed96739bd067db9d57627
BBecker <benedict.becker@rwth-aachen.de>
2016-06-28 14:15:19 +0200

    Added warning dialog

--- FaultTreeAnalysis.GUI/Windows/MessageDialogs.cs
+++ FaultTreeAnalysis.GUI/Windows/MessageDialogs.cs
@@ -15,6 +15,9 @@ namespace FaultTreeAnalysis.GUI.Windows
 		public static async Task<string> ShowRateDialogAsync()
 			=> await ShowInputDialogAsync("Rate", "Enter rate for edge", new MetroDialogSettings());
 
+		public static async Task<MessageDialogResult> ShowWarningAsync(string message)
+			=> await Core.MainWindow.ShowMessageAsync("Warning", message, MessageDialogStyle.AffirmativeAndNegative);
+
 	    public static async Task<ProgressDialogController> ShowProgressDialog(string title, string message, MetroDialogSettings settings) => 
             await Core.MainWindow.ShowProgressAsync(title, message, false, settings);
 
