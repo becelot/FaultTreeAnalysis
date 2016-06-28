214b3de07dbd1fd95f5668ecde9038dfcf561c53
BBecker <benedict.becker@rwth-aachen.de>
2016-06-28 20:07:39 +0200

    Added Hotkey definition

---
+++ FaultTreeAnalysis.GUI/Windows/MainWindow.Hotkeys.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FaultTreeAnalysis.GUI.Windows
+{
+    public partial class MainWindow
+    {
+        public static RoutedCommand AddAndGateCommand = new RoutedCommand { InputGestures = { new KeyGesture(Key.A, ModifierKeys.Control)}};
+        public static RoutedCommand AddOrGateCommand = new RoutedCommand { InputGestures = { new KeyGesture(Key.O, ModifierKeys.Control) } };
+        public static RoutedCommand AddBasicEventCommand = new RoutedCommand { InputGestures = { new KeyGesture(Key.B, ModifierKeys.Control) } };
+        public static RoutedCommand AddMarkovChainCommand = new RoutedCommand { InputGestures = { new KeyGesture(Key.M, ModifierKeys.Control) } };
+        public static RoutedCommand AddConnectionCommand = new RoutedCommand { InputGestures = { new KeyGesture(Key.C, ModifierKeys.Control) } };
+        public static RoutedCommand RemoveComponentCommand = new RoutedCommand { InputGestures = { new KeyGesture(Key.R, ModifierKeys.Control) } };
+    }
+}
