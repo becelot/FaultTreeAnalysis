3792e352e4398eb8291c0c116085cb74f96d075e
BBecker <benedict.becker@rwth-aachen.de>
2016-06-29 14:33:20 +0200

    Added initial distribution to markov chain

---
+++ FaultTreeAnalysis/FaultTree/MarkovChain/MarkovChain.Analysis.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MarkovChain.Analysis.cs" company="RWTH-Aachen">
+//   Benedict Becker, Nico Jansen
+// </copyright>
+// <summary>
+//   
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace FaultTreeAnalysis.FaultTree.MarkovChain
+{
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// The markov chain.
+    /// </summary>
+    public partial class MarkovChain<TVertex>
+    {
+        /// <summary>
+        /// Gets the initial distribution.
+        /// </summary>
+        [DataMember]
+        public Dictionary<TVertex, double> InitialDistribution { get; private set; } = new Dictionary<TVertex, double>();
+
+    }
+}
