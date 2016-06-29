83c008b4ebc72673b568dd4e71a0d9d487b05bbe
BBecker <benedict.becker@rwth-aachen.de>
2016-06-29 14:40:59 +0200

    Added Distribution converter

---
+++ FaultTreeAnalysis.GUI/Converters/TerminalNodeToDistributionConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaultTreeAnalysis.GUI.Converters
+{
+    using System.Globalization;
+    using System.Windows.Data;
+
+    using FaultTreeAnalysis.FaultTree.Tree;
+
+    public class TerminalNodeToDistributionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "Initial distribution: " + Core.MainWindow.ViewModel.FaultTree.MarkovChain.InitialDistribution[(FaultTreeTerminalNode)value];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
